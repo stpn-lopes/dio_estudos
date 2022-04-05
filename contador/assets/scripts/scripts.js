511f2b08a8d179da4e6f389bd35d7b2d3260d560
Stephany Lopes <stpn.lopes@gmail.com>
2022-04-04 22:20:26 -0300

    add function comment

--- contador/assets/scripts/scripts.js
+++ contador/assets/scripts/scripts.js
@@ -1,15 +1,35 @@
 let count
 let currentNumber = document.getElementById(display);
+let div = document.getElementById(divCommentary);
+
+function comment (){
+    if (count <4) {
+        document.getElementById("display").style.color = "black";
+        document.getElementById("display").style.backgroundColor = "red";
+    }
+    if (count <7){
+        document.getElementById("display").style.color = "black";
+        document.getElementById("display").style.backgroundColor = "yellow";
+    }
+    if (count >7){
+        document.getElementById("display").style.color = "black";
+        document.getElementById("display").style.backgroundColor = "green";
+    }
+}
 
 function decrement (){
     count = currentNumber--;
     display.innerHTML = count;
+    comment();
 }
 
 function increment (){
     count = currentNumber++;
     display.innerHTML = count;
+    comment();
 }
+
+
 
 function test (){
     console.log('hello world');
