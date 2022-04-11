50b121f63db1c88a89a6daa1250d15f24d234a1b
Stephany Lopes <stpn.lopes@gmail.com>
2022-04-11 20:57:06 -0300

    set snake moviments

--- html_web_developer/Projetos/recriando-jogo-cobrinha-js/assets/scripts.js
+++ html_web_developer/Projetos/recriando-jogo-cobrinha-js/assets/scripts.js
@@ -1,12 +1,14 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
-let box = 32;
-let snake = [];
+let box = 25;
 
+let snake = [];
 snake[0] = {
     x: 10 * box,
     y: 10 * box
 };
+
+let direction = "right";
 
 function addBG() {
     context.fillStyle = "black";
@@ -17,9 +19,31 @@ function addSnake (){
     for (i = 0; i < snake.length; i++) {
         context.fillStyle= 'tomato';
         context.fillRect(snake[i].x,snake[i].y, box, box);
-        
     }
 }
 
-addBG();
-addSnake();
+function start(){
+    addBG();
+    addSnake();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+    
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if (direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let game = setInterval (start, 100);
+
+start()
