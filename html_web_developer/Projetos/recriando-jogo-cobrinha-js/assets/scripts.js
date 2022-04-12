1a09cfb9e2fc9697358ec49eeb5f3d960a4735c6
Stephany Lopes <stpn.lopes@gmail.com>
2022-04-11 23:35:15 -0300

    set directions and limit

--- html_web_developer/Projetos/recriando-jogo-cobrinha-js/assets/scripts.js
+++ html_web_developer/Projetos/recriando-jogo-cobrinha-js/assets/scripts.js
@@ -1,18 +1,18 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
-let box = 25;
+let box = 32;
 
 let snake = [];
 snake[0] = {
-    x: 10 * box,
-    y: 10 * box
+    x: 8 * box,
+    y: 8 * box
 };
 
-let direction = "right";
+let direction = "left";
 
 function addBG() {
     context.fillStyle = "black";
-    context.fillRect(0, 0, 20 * box, 20 * box);
+    context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 function addSnake (){
@@ -22,7 +22,22 @@ function addSnake (){
     }
 }
 
+document.addEventListener('keydown', update);
+
+function update(event){
+    if(event.keyCode == 37 && direction != 'right') direction = 'left';
+    if(event.keyCode == 38 && direction != 'down') direction = 'up';
+    if(event.keyCode == 39 && direction != 'left') direction = 'right';
+    if(event.keyCode == 40 && direction != 'up') direction = 'down';
+}
+
 function start(){
+    if (snake[0].x > 15 *box && direction == "right") snake[0].x = 0;
+    if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if (snake[0].y > 15 *box && direction == "down") snake[0].y = 0;
+    if (snake[0].y > 0 && direction == "up") snake[0].y = 16 * box;
+    
+
     addBG();
     addSnake();
 
@@ -32,7 +47,7 @@ function start(){
     if(direction == "right") snakeX += box;
     if(direction == "left") snakeX -= box;
     if(direction == "up") snakeY -= box;
-    if (direction == "down") snakeY += box;
+    if(direction == "down") snakeY += box;
 
     snake.pop();
 
@@ -44,6 +59,6 @@ function start(){
     snake.unshift(newHead);
 }
 
-let game = setInterval (start, 100);
+let game = setInterval (start, 200);
 
-start()
+start();
